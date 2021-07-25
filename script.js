17ea139e66d4e9d3e9c49dcb1cba2ff171aa4752
sreecharanya <sree3charanya@gmail.com>
2021-07-25 00:09:05 -0500

    done

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+            const container = document.getElementById("container");
+            for (let astronaut of json) {
+                container.innerHTML += `
+                <div class = "astronaut">
+                    <div class = "bio">
+                        <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                            <li>Active: ${astronaut.active}</li>
+                            <li>Skills: ${astronaut.skills.join(", ")}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${astronaut.picture}"/>
+                </div>
+                `;
+            }
+        });
+    });
+});
+  
